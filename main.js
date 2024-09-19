55af97338c2028e83205a3a3f5d7960ac986227e
Gulnoza <khusinovagulnoza5@gmail.com>
2024-09-20 00:03:09 +0500

    base commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// function sale (){
+//   let saleDate = new Date(2024,8,25)
+//   let currentDate = new Date()
+//   let date = saleDate - currentDate
+//   let day = Math.floor(date/(24*60*60*1000))
+//   let hours = Math.floor((date%(24*60*60*1000))/(60*1000*60))
+//   let minutes =Math.floor((date%(60*60*1000))/(60*1000))
+//   let second = Math.floor((date%(60*1000))/1000)
+
+// let timeValue = document.getElementsByClassName("date")
+// timeValue[0].innerHTML = day
+// timeValue[1].innerHTML = hours
+// timeValue[2].innerHTML = minutes
+// timeValue[3].innerHTML = second
+// }
+
+// sale()
