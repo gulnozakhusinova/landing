aa8fb84a47a6bd5435993c055c6d909ff3325c31
Gulnoza <khusinovagulnoza5@gmail.com>
2024-09-22 14:04:09 +0500

    fix html and date

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
-// function sale (){
-//   let saleDate = new Date(2024,8,25)
-//   let currentDate = new Date()
-//   let date = saleDate - currentDate
-//   let day = Math.floor(date/(24*60*60*1000))
-//   let hours = Math.floor((date%(24*60*60*1000))/(60*1000*60))
-//   let minutes =Math.floor((date%(60*60*1000))/(60*1000))
-//   let second = Math.floor((date%(60*1000))/1000)
+function correctDate (date){
+  return date < 10 ? "0" + date :date
+}
 
-// let timeValue = document.getElementsByClassName("date")
-// timeValue[0].innerHTML = day
-// timeValue[1].innerHTML = hours
-// timeValue[2].innerHTML = minutes
-// timeValue[3].innerHTML = second
-// }
 
-// sale()
+
+function sale (){
+  let saleDate = new Date(2024,8,25)
+  let currentDate = new Date()
+  let date = saleDate - currentDate
+  let day = Math.floor(date/(24*60*60*1000))
+  let hours = Math.floor((date%(24*60*60*1000))/(60*1000*60))
+  let minutes =Math.floor((date%(60*60*1000))/(60*1000))
+  let second = Math.floor((date%(60*1000))/1000)
+let timeValue = document.getElementsByClassName("date")
+
+
+timeValue[0].innerHTML = correctDate(day)
+timeValue[1].innerHTML = correctDate(hours)
+timeValue[2].innerHTML = correctDate(minutes)
+timeValue[3].innerHTML =correctDate(second)
+
+if (date <= 0){
+
+  timeValue[0].innerHTML ="00"
+  timeValue[1].innerHTML ="00"
+  timeValue[2].innerHTML ="00"
+  timeValue[3].innerHTML ="00"
+
+}
+
+
+}
+
+sale() 
+
+setInterval(()=>{
+  sale()
+},1000)
